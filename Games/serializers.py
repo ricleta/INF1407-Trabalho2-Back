6c9b1fae6af28f47167675991c7dfae7d92f86a8
Ricardo Bastos Leta Vieira <47068518+ricleta@users.noreply.github.com>
2025-11-22 20:53:11 +0000

    feat: Add reviews field to GamesModelSerializer for enhanced game detail representation

--- Games/serializers.py
+++ Games/serializers.py
@@ -1,7 +1,4 @@
 from rest_framework import serializers
-from .models import GamesModel
-
-from rest_framework import serializers
 from django.contrib.auth.models import User # Import the User model
 from .models import GamesModel
 
@@ -13,9 +10,24 @@ class DeveloperSerializer(serializers.ModelSerializer):
 
 class GamesModelSerializer(serializers.ModelSerializer):
     # Tell the developer field to use the nested serializer.
-    # It's read-only because it's populated based on the authenticated user who creates the game.
     developer = DeveloperSerializer(read_only=True)
+    
+    # Add a field to explicitly fetch reviews
+    reviews = serializers.SerializerMethodField()
 
     class Meta:
         model = GamesModel
-        fields = ['id', 'title', 'platforms', 'description', 'release_date', 'developer']
+        # Add 'reviews' to the list of fields
+        fields = ['id', 'title', 'platforms', 'description', 'release_date', 'developer', 'reviews']
+
+    def get_reviews(self, obj):
+        # Fetch related reviews using the 'related_name="reviews"' defined in the ReviewModel
+        reviews = obj.reviews.all()
+        return [
+            {
+                "user": {"username": review.user.username},
+                "rating": review.rating,
+                "comment": review.comment
+            }
+            for review in reviews
+        ]
