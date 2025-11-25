289aa83e2cd2553005a112835125f96b003650c0
Ricardo Bastos Leta Vieira <47068518+ricleta@users.noreply.github.com>
2025-11-25 14:31:31 +0000

    changed email backend setting

--- AvaliacoesSite/settings.py
+++ AvaliacoesSite/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # LOGIN_REDIRECT_URL = 'home-page' # Where to go after successful login
 # LOGOUT_REDIRECT_URL = 'home-page' # Where to go after logout
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
