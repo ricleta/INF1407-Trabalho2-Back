ce9cd592a9b8e2ff2b4c56ceced187460fee6daa
Ricardo Bastos Leta Vieira <47068518+ricleta@users.noreply.github.com>
2025-11-25 14:17:06 +0000

    updated url

--- Seguranca/urls.py
+++ Seguranca/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', views.CustomAuthToken.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', views.UserRegistrationView.as_view(), name='user-registration'),
-    path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot-password'),
+    path('forgot_password/', views.ForgotPasswordView.as_view(), name='forgot-password'),
 ]
